834a87789999a2b91f4d4c0e039f3640f3384db4
Ola Assem <ola.assem@viget.com>
2021-07-17 23:07:38 +0200

    color options and dropdown comp + styling initial

--- src/App.js
+++ src/App.js
@@ -1,27 +1,35 @@
 // import Accordion from './components.js/Accordion';
+// import Search from './components.js/Search';
 
-import Search from './components.js/Search';
+import Dropdown from './components.js/Dropdown';
 
-// const items = [
-// 	{
-// 		title: 'What is React?',
-// 		content: 'React is a front end javascript framework',
-// 	},
-// 	{
-// 		title: 'Why use React?',
-// 		content: 'React is a favorite JS library among engineers',
-// 	},
-// 	{
-// 		title: 'How do you use React?',
-// 		content: 'You use React by creating components',
-// 	},
-// ];
+const items = [
+	{
+		title: 'What is React?',
+		content: 'React is a front end javascript framework',
+	},
+	{
+		title: 'Why use React?',
+		content: 'React is a favorite JS library among engineers',
+	},
+	{
+		title: 'How do you use React?',
+		content: 'You use React by creating components',
+	},
+];
+
+const options = [
+	{ label: 'Red', color: 'red' },
+	{ label: 'Green', color: 'green' },
+	{ label: 'Blue', color: 'blue' },
+];
 
 export default function App() {
 	return (
 		<div>
 			{/* <Accordion items={items} /> */}
-			<Search />
+			{/* <Search /> */}
+			<Dropdown options={options} />
 		</div>
 	);
 }
