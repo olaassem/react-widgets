6d69f21d5c726108139418c985627dc1de3bdb54
Ola Assem <ola.assem@viget.com>
2021-07-17 19:58:44 +0200

    onTitleClicked helper function

--- src/components.js/Accordion.js
+++ src/components.js/Accordion.js
@@ -1,10 +1,14 @@
 import React from 'react';
 
 export default function Accordion({ items }) {
-	const renderedItems = items.map((item) => {
+	const onTitleClicked = (i) => {
+		console.log(`Item ${i} is clicked!`);
+	};
+
+	const renderedItems = items.map((item, i) => {
 		return (
 			<React.Fragment key={item.title}>
-				<div className="title active">
+				<div className="title active" onClick={() => onTitleClicked(i)}>
 					<i className="dropdown icon"></i>
 					{item.title}
 				</div>
