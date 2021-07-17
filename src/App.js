e9951355eed08048a5abe5f6eca766d94adb3047
Ola Assem <ola.assem@viget.com>
2021-07-17 21:19:13 +0200

    installs axios & exwcutes search req

--- src/App.js
+++ src/App.js
@@ -2,20 +2,20 @@
 
 import Search from './components.js/Search';
 
-const items = [
-	{
-		title: 'What is React?',
-		content: 'React is a front end javascript framework',
-	},
-	{
-		title: 'Why use React?',
-		content: 'React is a favorite JS library among engineers',
-	},
-	{
-		title: 'How do you use React?',
-		content: 'You use React by creating components',
-	},
-];
+// const items = [
+// 	{
+// 		title: 'What is React?',
+// 		content: 'React is a front end javascript framework',
+// 	},
+// 	{
+// 		title: 'Why use React?',
+// 		content: 'React is a favorite JS library among engineers',
+// 	},
+// 	{
+// 		title: 'How do you use React?',
+// 		content: 'You use React by creating components',
+// 	},
+// ];
 
 export default function App() {
 	return (
