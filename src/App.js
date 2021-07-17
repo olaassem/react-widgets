1d5100e0f27db05d5c1e3fb94dcdede6d4ee5cb7
Ola Assem <ola.assem@viget.com>
2021-07-17 23:23:20 +0200

    filtering the options list

--- src/App.js
+++ src/App.js
@@ -4,25 +4,25 @@
 import { useState } from 'react';
 import Dropdown from './components.js/Dropdown';
 
-const items = [
-	{
-		title: 'What is React?',
-		content: 'React is a front end javascript framework',
-	},
-	{
-		title: 'Why use React?',
-		content: 'React is a favorite JS library among engineers',
-	},
-	{
-		title: 'How do you use React?',
-		content: 'You use React by creating components',
-	},
-];
+// const items = [
+// 	{
+// 		title: 'What is React?',
+// 		content: 'React is a front end javascript framework',
+// 	},
+// 	{
+// 		title: 'Why use React?',
+// 		content: 'React is a favorite JS library among engineers',
+// 	},
+// 	{
+// 		title: 'How do you use React?',
+// 		content: 'You use React by creating components',
+// 	},
+// ];
 
 const options = [
-	{ label: 'A Hint of Red', color: 'red' },
+	{ label: 'A Blush of Red', color: 'red' },
 	{ label: 'A Glimmer of Green', color: 'green' },
-	{ label: 'A Shade of Blue', color: 'blue' },
+	{ label: 'A Wave of Blue', color: 'blue' },
 ];
 
 export default function App() {
