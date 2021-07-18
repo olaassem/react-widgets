ac9a586d730e35fc90e2ff36cc3955bf2f2c9bfd
Ola Assem <ola.assem@viget.com>
2021-07-18 02:23:40 +0200

    adds the language input

--- src/components.js/Translate.js
+++ src/components.js/Translate.js
@@ -9,15 +9,27 @@ export default function Translate() {
 	];
 
 	const [language, setLanguage] = useState(options[0]);
+	const [text, setText] = useState('');
 
 	return (
 		<div>
-			<Dropdown
-				label="Select a Language"
-				selected={language}
-				onSelectedChange={setLanguage}
-				options={options}
-			/>
+			<div className="ui form">
+				<div className="field">
+					<label>Enter Text</label>
+					<input
+						type="text"
+						value={text}
+						onChange={(e) => setText(e.target.value)}
+					/>
+				</div>
+
+				<Dropdown
+					label="Select a Language"
+					selected={language}
+					onSelectedChange={setLanguage}
+					options={options}
+				/>
+			</div>
 		</div>
 	);
 }
